24e7651db882151ddcc628ff6023a1e7ff4852ad
Anastasiya <Zdorovets2008@yandex.ru>
2024-09-25 22:13:40 +0300

    knbc

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner sc = new Scanner(System.in);
+        String[] arr1 = {"Rock", "Spock", "Paper", "Lizard", "Scissors"};
+        Random rand = new Random();
+        boolean c = true;
+        int b=0;
+        String a;
+        while (c) {
+            System.out.print("Please, enter your choice: ");
+            a = sc.nextLine();
+            if ("Exit".equals(a)) {
+                c = false;
+            }
+            outer:
+            for (int i = 0; i < 5; ++i) {
+                if (arr1[i].equals(a)) {
+                    b = i;
+                    break outer;
+                }
+            }
+            int pc = rand.nextInt(5);
+            System.out.println("PC has chosen: " + arr1[pc]);
+            if (b == pc) {
+                System.out.println("Tie");
+            } else if (0 < b - pc && b - pc <= 2) {
+                System.out.println("Player win!");
+            } else if (0 > b - pc && b - pc <= -3) {
+                System.out.println("Player win!");
+            } else {
+                System.out.println("PC win!");
+            }
         }
+
     }
 }
